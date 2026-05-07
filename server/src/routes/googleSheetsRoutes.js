fcc982e235ab326f1a5472d5d70d5ed38f73206a
rahul.saini <rahul.saini@anuragdholegmail.onmicrosoft.com>
2026-05-07 16:57:19 +0000

    Deleted server

--- server/src/routes/googleSheetsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { addToSheet } from '../controllers/googleSheetsController.js';
-import { authenticateUser } from '../middleware/authMiddleware.js';
-
-const router = express.Router();
-
-router.post('/add', authenticateUser, addToSheet);
-
-export default router;
